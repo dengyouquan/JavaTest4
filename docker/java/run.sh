a654e55a5130a8d55677781e75264b770ee33fd9
dengyouquan <youquan.deng@hand-china.com>
2018-08-07 16:36:18 +0800

    second

--- docker/java/run.sh
+++ docker/java/run.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-set -e
-
-#sleep 15
 while ! nc -z ${MYSQL_IP} ${MYSQL_PORT}; do sleep 3; done
 #echo "${MYSQL_IP}:${MYSQL_PORT}"
 echo 'start run app.jar'
 java -jar app.jar
-
-#tail -f /dev/null
